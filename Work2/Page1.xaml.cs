321a61e6a786af24bdcbf1deaf888d3a74925aeb
pc202020 <pc202020@pc202020.ru>
2024-01-25 04:17:07 +0300

    Добавьте файлы проекта.

---
+++ Work2/Page1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Work2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void CompliteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(NumberTextBox.Text, out int number) || number < 100 || number > 999)
+            {
+                MessageBox.Show("Введите трёхзначное число!");
+                return;
+            }
+
+            int firstNumber = number / 100;
+            int secondNumber = number % 100 / 10;
+            int thirdNumber = number % 10;
+
+            int sum = firstNumber + secondNumber + thirdNumber;
+            int product = firstNumber * secondNumber * thirdNumber;
+
+            AnswerTextBox.Text = $"Сумма: {sum}, Произведение: {product}";
+        }
+    }
+}
---
+++ Work2/Page2.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Work2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void CompliteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(NumberTextBox.Text, out int number))
+            {
+                MessageBox.Show("Введите только число!");
+                return;
+            }
+
+            if (number > 0 && number < 366)
+            {
+                int day = number % 7;
+                if (day == 0)
+                {
+                    day = 7;
+                }
+
+                AnswerTextBox.Text = $"Номер дня недели: {day}";
+            }
+            else
+            {
+                AnswerTextBox.Text = "В году всего 365 дней, если он не високосный";
+            }
+        }
+    }
+}
